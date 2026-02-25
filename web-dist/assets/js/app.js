84d50b5f7e0bcd9565fab11370f80cdcc8e2f3c9
Simli <simlihac@gmail.com>
2026-02-25 14:43:56 +0900

    Improve Sudoku visibility and board centering for mobile/desktop

--- web-dist/assets/js/app.js
+++ web-dist/assets/js/app.js
@@ -1,18 +1,18 @@
-﻿import * as api from "./api.js?v=20260224-13";
-import { state, setCurrentDaily } from "./state.js?v=20260224-13";
-import { dom, setStatus, setModeInfo, setTimer, setPicrossModeButtons, renderLeaderboard } from "./ui.js?v=20260224-13";
+﻿import * as api from "./api.js?v=20260224-14";
+import { state, setCurrentDaily } from "./state.js?v=20260224-14";
+import { dom, setStatus, setModeInfo, setTimer, setPicrossModeButtons, renderLeaderboard } from "./ui.js?v=20260224-14";
 import {
   renderSudokuBoard,
   collectSudokuAnswer,
   setSelectedSudokuValue,
   clearSudokuInputs,
-} from "./puzzles/sudoku.js?v=20260224-13";
+} from "./puzzles/sudoku.js?v=20260224-14";
 import {
   renderPicrossBoard,
   collectPicrossAnswer,
   setPicrossInputMode,
   clearPicrossBoard,
-} from "./puzzles/picross.js?v=20260224-13";
+} from "./puzzles/picross.js?v=20260224-14";
 
 let timerHandle = null;
 
